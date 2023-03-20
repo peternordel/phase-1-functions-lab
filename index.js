a2ee6ea2141c5de92792c5677c186b3b0eb010f9
Peter Nordel <peternordel@gmail.com>
2023-03-20 16:00:18 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+    return Math.abs(destination - 42);
+}
+
+function distanceFromHqInFeet (destination) {
+    return distanceFromHqInBlocks(destination) * 264;
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return (Math.abs(start - destination) * 264);
+}
+
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return 0.02 * (distanceTravelledInFeet(start, destination) - 400);
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2500) {
+        return 25;
+    }
+    else return 'cannot travel that far';
+}
